66f2ebe4705a95601039423f3d256d90e1184283
mariacharadgipse <mariachara.dgipse@gmail.com>
2023-10-22 00:56:18 -0300

    Agrego practica agregando estilos

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/',(req, res)=>{
     res.sendFile(path.join(__dirname, 'views', 'home.html'))
+    //res.send('funciona')
 });
 
 const port=3000
