85b46afeb7cb4d609474b22128aa755d9d9327f2
mariacharadgipse <mariachara.dgipse@gmail.com>
2023-10-29 03:29:06 -0300

    Terminado 19 y empezado 20

--- src/app.js
+++ src/app.js
@@ -9,5 +9,16 @@ app.get('/',(req, res)=>{
     //res.send('funciona')
 });
 
+
+app.get('*', (req, res)=>{
+    res.send(`
+    <div style="display:flex; flex-direction:column; align-items:center">
+    <h1>Esta pagina no existe</h1>
+    <img style="width:50%" src="/img/not-found.webp" alt="error-404">
+    </div>  
+    `)
+  })
+  
+
 const port=3000
 app.listen(port, ()=>console.log(`Servidor corriendo http://localhost:${port}`));
